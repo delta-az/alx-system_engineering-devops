f27ab782d63b0b2a8e40b012f9cca9bd58ecf452
delta-az <azedinedelta@gmail.com>
2024-01-24 04:04:27 -0800

    r

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
+# Puppet script to create ssh config file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  }
+}
+
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
-  }
+  line   => '    IdentityFile ~/.ssh/school',
+}
